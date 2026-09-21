25135dd89e9853ffd89fb7116f6fec8d9211632f
Mehedi H Joy <mh506370@gmail.com>
2026-09-21 04:18:26 +0000

    Revert "input: sec_ts: Use KEY_WAKEUP for DT2W"
    This reverts commit 0e7672034c9a943b45d5c75fbf03b8755b822677.

--- drivers/input/touchscreen/sec_ts/y771/sec_ts.c
+++ drivers/input/touchscreen/sec_ts/y771/sec_ts.c
@@ -1590,10 +1590,7 @@ static void sec_ts_read_event(struct sec_ts_data *ts)
 					if (p_gesture_status->gesture_id == SEC_GESTURE_ID_FOD_LONG || p_gesture_status->gesture_id == SEC_GESTURE_ID_FOD_NORMAL) {
 						ts->scrub_id = SPONGE_EVENT_TYPE_FOD;
 						input_info(true, &ts->client->dev, "%s: FOD: %s\n", __func__, p_gesture_status->gesture_id ? "normal" : "long");
-					if (!is_aosp)
 						input_report_key(ts->input_dev, KEY_BLACK_UI_GESTURE, 1);
-					ts->fod_pressed = true;
-					sysfs_notify(&ts->input_dev->dev.kobj, NULL, "fod_pressed");
 					} else if (p_gesture_status->gesture_id == SEC_GESTURE_ID_FOD_RELEASE) {
 						ts->scrub_id = SPONGE_EVENT_TYPE_FOD_RELEASE;
 						input_info(true, &ts->client->dev, "%s: FOD release\n", __func__);
